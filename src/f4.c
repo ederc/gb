6ca2e92dec9958b33457a5bc3afe2003ba24973f
Christian Eder <ederc@mathematik.uni-kl.de>
2017-12-19 11:24:34 +0100

    adds more plain files, configure and make works, but nothing is really built

---
+++ src/f4.c
@@ -0,0 +1,31 @@
+/* gb: Gröbner Basis
+ * Copyright (C) 2015 Christian Eder <ederc@mathematik.uni-kl.de>
+ * This file is part of gb.
+ * gbla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ * gbla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with gbla . If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file f4.c
+ * \brief Implementation of the F4 Algorithm
+ *
+ * \author Christian Eder <ederc@mathematik.uni-kl.de>
+ */
+
+#include "data.h"
+
+static ? f4(???)
+{
+  initialize_statistics();
+  initialize_basis();
+  initialize_pairset();
+  initialize_hashtable();
+}

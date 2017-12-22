84d7ff5232b2c533fb926139cd22ac73c4942100
Christian Eder <ederc@mathematik.uni-kl.de>
2017-12-22 11:09:23 +0100

    adds testsuite behaviour

--- test/test_julia_io.c
+++ test/test_julia_io.c
@@ -7,8 +7,6 @@ int main(
     void
     )
 {
-  printf("[TEST] Julia IO\n");
-
   const int32_t lens[]  = {2,2,2}; 
   const int32_t cfs[]   = {1, 1, 1, 3, 5, 3};
   const int32_t exps[]  = {1, 0, 0, 1, 3, 0, 0, 1, 1, 1, 1, 0};

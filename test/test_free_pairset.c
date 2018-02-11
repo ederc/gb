16f157dfd27563691b9f2810fa3c99850b50a6d8
Christian Eder <ederc@mathematik.uni-kl.de>
2018-02-11 15:16:03 +0100

    adds test files

---
+++ test/test_free_pairset.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include "../src/gb.c"
+
+int main(
+    void
+    )
+{
+  /* initialize stuff */
+  initialize_pairset();
+  free_pairset();
+  if (ps != NULL) {
+    return 1;
+  }
+  if (pload != 0) {
+    return 1;
+  }
+  if (psize != 0) {
+    return 1;
+  }
+  return 0;
+}

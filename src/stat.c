28b320ef549bd4d8ca246c1e588d25132ddddea5
Christian Eder <ederc@mathematik.uni-kl.de>
2018-12-17 16:20:46 +0100

    fixes warning for number of matrices reduced

--- src/stat.c
+++ src/stat.c
@@ -161,7 +161,7 @@ void print_final_statistics(
     printf("#pairs reduced     %9ld\n", st->num_pairsred);
     printf("#GM criterion      %9ld\n", st->num_gm_crit);
     printf("#redundant         %9ld\n", st->num_redundant);
-    printf("#matrices reduced  %9ld\n", st->num_matrices);
+    printf("#matrices reduced  %9d\n", st->num_matrices);
     printf("#rows reduced      %9ld\n", st->num_rowsred);
     printf("#zero reductions   %9ld\n", st->num_zerored);
     printf("#global hash table %9d <= 2^%d\n",
